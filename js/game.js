4043cd5c2a3c8533480b6ca4f706e27b41cb9d7a
blakelower <blakelower1999@gmail.com>
2020-02-29 13:47:57 +0700

    update

--- js/game.js
+++ js/game.js
@@ -1,3 +1,306 @@
+// const KEY_CODE_LEFT = 37;
+// const KEY_CODE_RIGHT = 39;
+// const KEY_CODE_SPACE = 32;
+
+// const GAME_WIDTH = 800;
+// const GAME_HEIGHT = 600;
+
+// const PLAYER_WIDTH = 20;
+// const PLAYER_MAX_SPEED = 400.0; //how fast should the player move?
+// const LASER_MAX_SPEED = 400.0;
+// const LASER_COOLDOWN = 0.7;
+
+// const ENEMIES_PER_ROW = 6;
+// const ENEMY_HORIZONTAL_PADDING = 80;
+// const ENEMY_VERTICAL_PADDING = 70;
+// const ENEMY_VERTICAL_SPACING = 80;
+// const ENEMY_COOLDOWN = 5.0;
+
+// const GAME_STATE = {
+//   lastTime: Date.now(),
+//   leftPressed: false,
+//   rightPressed: false,
+//   spacePressed: false,
+//   playerX: 0,
+//   playerY: 0,
+//   playerCooldown: 0,
+//   lasers: [],
+//   enemies: [],
+//   enemyLasers: [],
+//   gameOver: false
+// };
+
+// var lives=0;
+// var score=0;
+
+// function rectsIntersect(r1, r2) {
+//   return !(
+//     r2.left > r1.right ||
+//     r2.right < r1.left ||
+//     r2.top > r1.bottom ||
+//     r2.bottom < r1.top
+//   );
+// }
+
+// function setPosition(el, x, y) {
+//   el.style.transform = `translate(${x}px, ${y}px)`;
+// }
+
+// function clamp(v, min, max) { //created clamp so the player does not go off the screen. correctly there should be divided by 2 so it is perfect, but small margin is okay and a bit stylish. 
+//   if (v < min) {
+//     return min;
+//   } else if (v > max) {
+//     return max;
+//   } else {
+//     return v;
+//   }
+// }
+
+
+// function rand(min, max) {
+//   if (min === undefined) min = 0;
+//   if (max === undefined) max = 1;
+//   return min + Math.random() * (max - min);
+// }
+
+// function createPlayer($container) {
+//   GAME_STATE.playerX = GAME_WIDTH / 2;
+//   GAME_STATE.playerY = GAME_HEIGHT - 50;
+//   const $player = document.createElement("img");
+//   $player.src = "img/unnamed.png";
+//   $player.className = "player";
+//   $container.appendChild($player);
+//   setPosition($player, GAME_STATE.playerX, GAME_STATE.playerY);
+// }
+
+// function destroyPlayer($container, player) {
+//   $container.removeChild(player);
+//   GAME_STATE.gameOver = true;
+//   const audio = new Audio("sound/starwars.ogg");
+//   audio.play();
+// }
+
+// function updatePlayer(dt, $container) { 
+//   if (GAME_STATE.leftPressed) {
+//     GAME_STATE.playerX -= dt * PLAYER_MAX_SPEED;
+//   }
+//   if (GAME_STATE.rightPressed) {
+//     GAME_STATE.playerX += dt * PLAYER_MAX_SPEED;
+//   }
+
+//   GAME_STATE.playerX = clamp( 
+//     GAME_STATE.playerX,
+//     PLAYER_WIDTH,
+//     GAME_WIDTH - PLAYER_WIDTH
+//   );
+
+//   if (GAME_STATE.spacePressed && GAME_STATE.playerCooldown <= 0) {
+//     createLaser($container, GAME_STATE.playerX, GAME_STATE.playerY);
+//     GAME_STATE.playerCooldown = LASER_COOLDOWN;
+//   }
+//   if (GAME_STATE.playerCooldown > 0) {
+//     GAME_STATE.playerCooldown -= dt;
+//   }
+
+//   const player = document.querySelector(".player");
+//   setPosition(player, GAME_STATE.playerX, GAME_STATE.playerY);
+// }
+
+// function createLaser($container, x, y) {
+//   const $element = document.createElement("img");
+//   $element.src = "img/missile1.png";
+//   $element.className = "laser";
+//   $container.appendChild($element);
+//   const laser = { x, y, $element };
+//   GAME_STATE.lasers.push(laser);
+//   const audio = new Audio("sound/laser.ogg");
+//   audio.play();
+//   setPosition($element, x, y);
+// }
+
+// function updateLasers(dt, $container) {
+//   const lasers = GAME_STATE.lasers;
+//   for (let i = 0; i < lasers.length; i++) {
+//     const laser = lasers[i];
+//     laser.y -= dt * LASER_MAX_SPEED;
+//     if (laser.y < 0) {
+//       destroyLaser($container, laser);
+//     }
+//     setPosition(laser.$element, laser.x, laser.y);
+//     const r1 = laser.$element.getBoundingClientRect();
+//     const enemies = GAME_STATE.enemies;
+//     for (let j = 0; j < enemies.length; j++) {
+//       const enemy = enemies[j];
+//       if (enemy.isDead) continue;
+//       const r2 = enemy.$element.getBoundingClientRect();
+//       if (rectsIntersect(r1, r2)) {
+//         // Enemy was hit
+//         destroyEnemy($container, enemy);
+//         destroyLaser($container, laser);
+//         break;
+//       }
+//     }
+//   }
+//   GAME_STATE.lasers = GAME_STATE.lasers.filter(e => !e.isDead);
+// }
+
+// //rename to make sense
+// function destroyLaser($container, laser) {
+//   $container.removeChild(laser.$element);
+//   laser.isDead = true;
+// }
+
+// function createEnemy($container, x, y) {
+//   const $element = document.createElement("img");
+//   $element.src = "img/tiee.png";
+//   $element.className = "enemy";
+//   $container.appendChild($element);
+//   const enemy = {
+//     x,
+//     y,
+//     cooldown: rand(0.5, ENEMY_COOLDOWN),
+//     $element
+//   };
+//   GAME_STATE.enemies.push(enemy);
+//   setPosition($element, x, y);
+// }
+
+// function updateEnemies(dt, $container) {
+//   const dx = Math.sin(GAME_STATE.lastTime / 1000.0) * 50;
+//   const dy = Math.cos(GAME_STATE.lastTime / 1000.0) * 10;
+
+//   const enemies = GAME_STATE.enemies;
+//   for (let i = 0; i < enemies.length; i++) {
+//     const enemy = enemies[i];
+//     const x = enemy.x + dx;
+//     const y = enemy.y + dy;
+//     setPosition(enemy.$element, x, y);
+//     enemy.cooldown -= dt;
+//     if (enemy.cooldown <= 0) {
+//       createEnemyLaser($container, x, y);
+//       enemy.cooldown = ENEMY_COOLDOWN;
+//     }
+//   }
+//   GAME_STATE.enemies = GAME_STATE.enemies.filter(e => !e.isDead);
+// }
+
+// function destroyEnemy($container, enemy) {
+//   $container.removeChild(enemy.$element);
+//   enemy.isDead = true;
+//   score += 35;
+//   document.getElementById('score').innerHTML = score;
+//   document.getElementById('total').innerHTML = score;
+// }
+
+// function createEnemyLaser($container, x, y) {
+//   const $element = document.createElement("img");
+//   $element.src = "img/missile2.png";
+//   $element.className = "enemy-laser";
+//   $container.appendChild($element);
+//   const laser = { x, y, $element };
+//   GAME_STATE.enemyLasers.push(laser);
+//   setPosition($element, x, y);
+// }
+
+// function updateEnemyLasers(dt, $container) {
+//   const lasers = GAME_STATE.enemyLasers;
+//   for (let i = 0; i < lasers.length; i++) {
+//     const laser = lasers[i];
+//     laser.y += dt * LASER_MAX_SPEED;
+//     if (laser.y > GAME_HEIGHT) {
+//       destroyLaser($container, laser);
+//     }
+//     setPosition(laser.$element, laser.x, laser.y);
+//     const r1 = laser.$element.getBoundingClientRect();
+//     const player = document.querySelector(".player");
+//     const r2 = player.getBoundingClientRect();
+//     if (rectsIntersect(r1, r2)) {
+//       // Player was hit
+//       lives -= 1;
+//       document.getElementById('lives').innerHTML = lives;
+//       if(lives == 0){
+//       destroyPlayer($container, player);
+//       break;
+//       }
+//     }
+//   }
+//   GAME_STATE.enemyLasers = GAME_STATE.enemyLasers.filter(e => !e.isDead);
+// }
+
+// function init() {
+//   lives = 5;
+//   score = 0;
+//   document.getElementById('lives').innerHTML = lives;
+//   document.getElementById('score').innerHTML = score;
+//   const $container = document.querySelector(".game");
+//   createPlayer($container);
+
+//   const enemySpacing =
+//     (GAME_WIDTH - ENEMY_HORIZONTAL_PADDING * 2) / (ENEMIES_PER_ROW - 1);
+//   for (let j = 0; j < 3; j++) {
+//     const y = ENEMY_VERTICAL_PADDING + j * ENEMY_VERTICAL_SPACING;
+//     for (let i = 0; i < ENEMIES_PER_ROW; i++) {
+//       const x = i * enemySpacing + ENEMY_HORIZONTAL_PADDING;
+//       createEnemy($container, x, y);
+//     }
+//   }
+// }
+
+// function playerHasWon() {
+//   return GAME_STATE.enemies.length === 0;
+// }
+
+
+// function update(e) { //passed all my state thru here
+//   const currentTime = Date.now(); //took current time to date.now but we do not want the absolute time. 
+//   const dt = (currentTime - GAME_STATE.lastTime) / 1000.0; //dt is delta time. dt contains the time difference between the beginning of the prev. frame and the beginning of the current frame in milliseconds. mainly used for creating time sensivtie animations i.e; our player moving. 
+
+//   if (GAME_STATE.gameOver) {
+//     document.querySelector(".game-over").style.display = "block";
+//     return;
+//   }
+
+//   if (playerHasWon()) {
+//     document.querySelector(".congratulations").style.display = "block";
+//     return;
+//   }
+ 
+//   const $container = document.querySelector(".game");
+//   updatePlayer(dt, $container);
+//   updateLasers(dt, $container);
+//   updateEnemies(dt, $container);
+//   updateEnemyLasers(dt, $container);
+
+//   GAME_STATE.lastTime = currentTime;
+//   window.requestAnimationFrame(update); //window animation is mainly used for games in JS
+// }
+
+
+// function onKeyDown(e) { //setting key state lines 275-296 are very similar all that I am doing is setting the key up/down by using a boolean so when the player plays they are able to use these functions.
+//   if (e.keyCode === KEY_CODE_LEFT) {
+//     GAME_STATE.leftPressed = true;
+//   } else if (e.keyCode === KEY_CODE_RIGHT) {
+//     GAME_STATE.rightPressed = true;
+//   } else if (e.keyCode === KEY_CODE_SPACE) {
+//     GAME_STATE.spacePressed = true;
+//   }
+// }
+
+// function onKeyUp(e) {
+//   if (e.keyCode === KEY_CODE_LEFT) {
+//     GAME_STATE.leftPressed = false;
+//   } else if (e.keyCode === KEY_CODE_RIGHT) {
+//     GAME_STATE.rightPressed = false;
+//   } else if (e.keyCode === KEY_CODE_SPACE) {
+//     GAME_STATE.spacePressed = false;
+//   } 
+// }
+
+// init();
+// window.addEventListener("keydown", onKeyDown);
+// window.addEventListener("keyup", onKeyUp);
+// window.requestAnimationFrame(update); //called the update to have it be in use
+
 const KEY_CODE_LEFT = 37;
 const KEY_CODE_RIGHT = 39;
 const KEY_CODE_SPACE = 32;
@@ -6,20 +309,18 @@ const KEY_CODE_PAUSE = 80;
 const GAME_WIDTH = 800;
 const GAME_HEIGHT = 600;
 
-
 const PLAYER_WIDTH = 20;
-const PLAYER_MAX_SPEED = 400.0; //how fast should the player move?
-const LASER_MAX_SPEED = 400.0;
-const LASER_COOLDOWN = 0.7;
+const PLAYER_MAX_SPEED = 600.0;
+const LASER_MAX_SPEED = 300.0;
+const LASER_COOLDOWN = 0.5;
 
-const ENEMIES_PER_ROW = 6;
+const ENEMIES_PER_ROW = 10;
 const ENEMY_HORIZONTAL_PADDING = 80;
 const ENEMY_VERTICAL_PADDING = 70;
 const ENEMY_VERTICAL_SPACING = 80;
 const ENEMY_COOLDOWN = 5.0;
 
 const GAME_STATE = {
-  //Global Game State
   lastTime: Date.now(),
   leftPressed: false,
   rightPressed: false,
@@ -31,10 +332,11 @@ const GAME_STATE = {
   lasers: [],
   enemies: [],
   enemyLasers: [],
-  gameOver: false,
-  //playerLives: 2,
+  gameOver: false
 };
 
+var lives=0;
+var score=0;
 
 function rectsIntersect(r1, r2) {
   return !(
@@ -49,7 +351,7 @@ function setPosition(el, x, y) {
   el.style.transform = `translate(${x}px, ${y}px)`;
 }
 
-function clamp(v, min, max) { //created clamp so the player does not go off the screen. correctly there should be divided by 2 so it is perfect, but small margin is okay and a bit stylish. 
+function clamp(v, min, max) {
   if (v < min) {
     return min;
   } else if (v > max) {
@@ -58,7 +360,6 @@ function clamp(v, min, max) { //created clamp so the player does not go off the 
     return v;
   }
 }
-
 
 function rand(min, max) {
   if (min === undefined) min = 0;
@@ -79,11 +380,11 @@ function createPlayer($container) {
 function destroyPlayer($container, player) {
   $container.removeChild(player);
   GAME_STATE.gameOver = true;
-  const audio = new Audio("sound/starwars.ogg");
+  const audio = new Audio("sound/sfx-lose.ogg");
   audio.play();
 }
 
-function updatePlayer(dt, $container) { 
+function updatePlayer(dt, $container) {
   if (GAME_STATE.leftPressed) {
     GAME_STATE.playerX -= dt * PLAYER_MAX_SPEED;
   }
@@ -91,7 +392,7 @@ function updatePlayer(dt, $container) {
     GAME_STATE.playerX += dt * PLAYER_MAX_SPEED;
   }
 
-  GAME_STATE.playerX = clamp( 
+  GAME_STATE.playerX = clamp(
     GAME_STATE.playerX,
     PLAYER_WIDTH,
     GAME_WIDTH - PLAYER_WIDTH
@@ -111,12 +412,12 @@ function updatePlayer(dt, $container) {
 
 function createLaser($container, x, y) {
   const $element = document.createElement("img");
-  $element.src = "img/missile1.png";
+  $element.src = "img/laser-blue-1.png";
   $element.className = "laser";
   $container.appendChild($element);
   const laser = { x, y, $element };
   GAME_STATE.lasers.push(laser);
-  const audio = new Audio("sound/laser.ogg");
+  const audio = new Audio("sound/sfx-laser1.ogg");
   audio.play();
   setPosition($element, x, y);
 }
@@ -147,7 +448,7 @@ function updateLasers(dt, $container) {
   GAME_STATE.lasers = GAME_STATE.lasers.filter(e => !e.isDead);
 }
 
-//rename to make sense
+// //rename to make sense
 function destroyLaser($container, laser) {
   $container.removeChild(laser.$element);
   laser.isDead = true;
@@ -155,7 +456,7 @@ function destroyLaser($container, laser) {
 
 function createEnemy($container, x, y) {
   const $element = document.createElement("img");
-  $element.src = "img/enemy.png";
+  $element.src = "img/tiee.png";
   $element.className = "enemy";
   $container.appendChild($element);
   const enemy = {
@@ -190,6 +491,9 @@ function updateEnemies(dt, $container) {
 function destroyEnemy($container, enemy) {
   $container.removeChild(enemy.$element);
   enemy.isDead = true;
+  score += 50;
+  document.getElementById('score').innerHTML = score;
+  document.getElementById('total').innerHTML = score;
 }
 
 function createEnemyLaser($container, x, y) {
@@ -216,14 +520,23 @@ function updateEnemyLasers(dt, $container) {
     const r2 = player.getBoundingClientRect();
     if (rectsIntersect(r1, r2)) {
       // Player was hit
-      destroyPlayer($container, player);
-      break;
+      lives -= 1;
+      document.getElementById('lives').innerHTML = lives;
+      destroyLaser($container, laser);
+      if(lives == 0){
+        destroyPlayer($container, player);
+        break;
+      }
     }
   }
   GAME_STATE.enemyLasers = GAME_STATE.enemyLasers.filter(e => !e.isDead);
 }
 
 function init() {
+  lives = 5;
+  score = 0;
+  document.getElementById('lives').innerHTML = lives;
+  document.getElementById('score').innerHTML = score;
   const $container = document.querySelector(".game");
   createPlayer($container);
 
@@ -242,10 +555,9 @@ function playerHasWon() {
   return GAME_STATE.enemies.length === 0;
 }
 
-
-function update(e) { //passed all my state thru here
-  const currentTime = Date.now(); //took current time to date.now but we do not want the absolute time. 
-  const dt = (currentTime - GAME_STATE.lastTime) / 1000.0; //dt is delta time. dt contains the time difference between the beginning of the prev. frame and the beginning of the current frame in milliseconds. mainly used for creating time sensivtie animations i.e; our player moving. 
+function update(e) {
+  const currentTime = Date.now();
+  const dt = (currentTime - GAME_STATE.lastTime) / 1000.0;
 
   if (GAME_STATE.gameOver) {
     document.querySelector(".game-over").style.display = "block";
@@ -256,11 +568,14 @@ function update(e) { //passed all my state thru here
     document.querySelector(".congratulations").style.display = "block";
     return;
   }
- 
   if (GAME_STATE.pausePressed) {
     document.querySelector(".pause").style.display = "block";
     return;
+  } if (GAME_STATE.pausePressed) {
+    document.querySelector(".btn").style.display = "block";
+    return;
   }
+
   const $container = document.querySelector(".game");
   updatePlayer(dt, $container);
   updateLasers(dt, $container);
@@ -268,11 +583,10 @@ function update(e) { //passed all my state thru here
   updateEnemyLasers(dt, $container);
 
   GAME_STATE.lastTime = currentTime;
-  window.requestAnimationFrame(update); //window animation is mainly used for games in JS
+  window.requestAnimationFrame(update);
 }
 
-
-function onKeyDown(e) { //setting key state lines 275-296 are very similar all that I am doing is setting the key up/down by using a boolean so when the player plays they are able to use these functions.
+function onKeyDown(e) {
   if (e.keyCode === KEY_CODE_LEFT) {
     GAME_STATE.leftPressed = true;
   } else if (e.keyCode === KEY_CODE_RIGHT) {
@@ -292,11 +606,11 @@ function onKeyUp(e) {
   } else if (e.keyCode === KEY_CODE_SPACE) {
     GAME_STATE.spacePressed = false;
   } else if (e.keyCode === KEY_CODE_PAUSE) {
-    GAME_STATE.pausePressed = false;
+      GAME_STATE.pausePressed = false;
   }
 }
 
 init();
 window.addEventListener("keydown", onKeyDown);
 window.addEventListener("keyup", onKeyUp);
-window.requestAnimationFrame(update); //called the update to have it be in use
+window.requestAnimationFrame(update);
